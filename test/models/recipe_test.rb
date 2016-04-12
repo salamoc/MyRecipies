6324699e82e006c78a421e5b7f526da1f021b64e
samuel <salamoc@gmail.com>
2016-04-12 09:15:20 +0000

    after first recipe tests

---
+++ test/models/recipe_test.rb
@@ -0,0 +1,68 @@
+require "test_helper"
+
+class RecipeTest <ActiveSupport::TestCase
+  
+  def setup
+    @recipe = Recipe.new(name: "chicken parm", summary: "this is the best chicken parm recipe ever",
+              description: "heat oil, add onions, add tomate sauce, add chicken, cook for 20 minutes")
+  end
+  
+  test "recipe should be valid" do
+    assert @recipe.valid?
+  end
+  
+  test "name should be present" do
+    @recipe.name = " "
+    assert_not @recipe.valid?
+  end
+  
+  test "name length should not be too long" do
+    @recipe.name = "a" * 101
+    assert_not @recipe.valid?
+  end
+  
+  test "name length should not be too short" do
+     @recipe.name = "aaaa"
+     assert_not @recipe.valid?
+  end
+  
+  test "summary must be present" do
+    @recipe.summary= " "
+    assert_not @recipe.valid?
+  end
+  
+  test "summary length must be not too long" do
+    @recipe.summary = "a" * 151
+    assert_not @recipe.valid?
+  end
+  
+  test "summary length must not bee too short" do
+    @recipe.summary = "a" * 9
+    assert_not @recipe.valid?
+  end
+  
+  test "description must be present" do
+    @recipe.description = " "
+    assert_not @recipe.valid?
+  end
+  
+  test "description length must not bee too long" do
+    @recipe.description = "a" * 501
+    assert_not @recipe.valid?
+  end
+  
+  test "description length must not bee too short" do
+    @recipe.description = "a" * 19
+    assert_not @recipe.valid?
+  end
+  
+end
+
+
+
+
+
+
+
+
+
